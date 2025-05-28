eba15763e0e4ee917b417b18b6ea657d120f117c
abdolrhman-mo <abdelrhmanlearn@gmail.com>
2025-05-28 11:43:01 +0300

    feat(ui): add logo icon (repeat) as the website favicon

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -3,4 +3,18 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'TaskLoop',
   description: 'Share and manage tasks with friends',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.svg',
+        type: 'image/svg+xml',
+      }
+    ],
+    apple: [
+      {
+        url: '/favicon.svg',
+        type: 'image/svg+xml',
+      }
+    ]
+  }
 }; 
